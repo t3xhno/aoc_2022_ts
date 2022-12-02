75af8c6df04e7a9361bb5690c9440228e6185830
tex <t3xhnolainx@gmail.com>
2022-12-02 20:33:43 +0100

    day02 | Check

--- day02/day02.ts
+++ day02/day02.ts
@@ -14,3 +14,5 @@ const sumResults = compose.all(sum, map(view(lensProp("result"))), map(evalRound
 const solve = t => compose.all(sumResults, makeInput(t));
 
 compose.all(tLog, Comb(solve(1), solve(2)), fetchDataRows())(INPUT_SRC);
+
+console.log("no");
