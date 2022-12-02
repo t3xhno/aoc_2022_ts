7bf977da118d11a2aa52c2ab43e4fdce5148e4fb
tex <t3xhnolainx@gmail.com>
2022-12-02 10:29:03 +0100

    AOC2022 | Deduplicate day02 solve

--- day02/day02.ts
+++ day02/day02.ts
@@ -2,15 +2,15 @@ const compose = require("folktale/core/lambda/compose");
 const { split, map, assoc, lensProp, sum, view } = require("ramda");
 const { tLog, Comb, fetchDataRows, INPUT_SRC, S, C, resMap, decryptMap, pointsMap } = require("../lib/index.ts");
 
-const getPick = r => r[1] === "Y" ? r[0] : r[1] === "X" ? resMap[r[0]].w : resMap[r[0]].l;
-const makeObj1 = r => ({ "op": r[0], "me": decryptMap[r[1]] });
-const makeObj2 = r => ({ "op": r[0], "me": getPick(r) });
 const evalWinner = (op, me) => op === me ? 3 : resMap[op].w === me ? 0 : 6;
 const calcResult = ({ op, me }) => pointsMap[me] + evalWinner(op, me);
-
 const evalRound = S(C(assoc("result")))(calcResult);
 
-const solve1 = compose.all(sum, map(view(lensProp("result"))), map(evalRound), map(makeObj1), map(split(/\s/g)));
-const solve2 = compose.all(sum, map(view(lensProp("result"))), map(evalRound), map(makeObj2), map(split(/\s/g)));
+const getPick = r => r[1] === "Y" ? r[0] : r[1] === "X" ? resMap[r[0]].w : resMap[r[0]].l;
+const makeObj = t => r => ({ "op": r[0], "me": t === 1 ? decryptMap[r[1]] : getPick(r) });
+const makeInput = t => compose(map(makeObj(t)), map(split(/\s/g)));
+const sumResults = compose.all(sum, map(view(lensProp("result"))), map(evalRound));
 
-compose.all(tLog, Comb(solve1, solve2), fetchDataRows())(INPUT_SRC);
+const solve = (t) => compose.all(sumResults, makeInput(t));
+
+compose.all(tLog, Comb(solve(1), solve(2)), fetchDataRows())(INPUT_SRC);
