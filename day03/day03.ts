0c5153aae11e1f2f76f6cc67e69b4bb4f5ab3ad3
tex <t3xhnolainx@gmail.com>
2022-12-04 16:33:53 +0100

    day03 | Use better functions

--- day03/day03.ts
+++ day03/day03.ts
@@ -1,11 +1,11 @@
 const { fetchDataRows, tLog, INPUT_SRC, Comb  } = require("../lib/index.ts");
 const { compose, map, indexOf, flip, sum, splitEvery } = require("ramda");
 
-const splitPacks = map(e => [e.slice(0, e.length / 2), e.slice(e.length / 2,)])
-const mapCommon = map(e => e[1].split("").filter(d => e[0].includes(d))[0]);
+const splitPacks = map(e => splitEvery(e.length / 2)(e));
+const mapCommon = map(e => e[1].split("").find(d => e[0].includes(d)));
 const scoreType = map(sum, map(flip(indexOf)(" abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")));
 const splitGroups = splitEvery(3);
-const findBadge = map(a => a[0].split("").filter(b => a[1].includes(b) && a[2].includes(b))[0]);
+const findBadge = map(a => a[0].split("").find(b => a[1].includes(b) && a[2].includes(b)));
 
 const solve1 = compose(scoreType, mapCommon, splitPacks);
 const solve2 = compose(scoreType, findBadge, splitGroups);
